da0663d09932248c1ef9319ae2ccd848eb1f2915
Griegoriens <skwarnel@gmail.com>
2022-08-15 15:55:01 +0200

    Finished: creating an array of tasks copied from .csv file

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -1,14 +1,26 @@
 package pl.coderslab;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TaskManager {
-
+    //developed on main branch
     public static void main(String[] args) {
+//        String[][] tasksList = new String[0][0];
+//        tasksList = writeTasksToArray("target/tasks.csv");
+//        printTasks(tasksList);
         mainRun();
+
+
     }
 
     public static void mainRun() {
+        String[][] tasksList = new String[0][0];
+        tasksList = writeTasksToArray("target/tasks.csv");
+        printTasks(tasksList);
+
         Scanner scan = new Scanner(System.in);
         String line = "";
         String[] options = {"add", "remove", "list", "select"};
@@ -46,5 +58,34 @@ public class TaskManager {
         for (int i = 0; i < arr.length; i++) {
             System.out.println(ConsoleColors.CYAN_BRIGHT + " -- " + arr[i] + ConsoleColors.RESET);
         }
+    }
+
+    public static void printTasks(String [][] tasksList) {
+        for (int i = 0; i < tasksList.length; i++) {
+            System.out.println("Arrays.toString(tasksList[i]) = " + Arrays.toString(tasksList[i]));
+        }
+        System.out.println("tasksList.length = " + tasksList.length);
+    }
+
+    public static String[][] writeTasksToArray(String fileName) {
+        File file = new File(fileName);
+        String line;
+        Scanner scan = new Scanner(System.in);
+        String[][] tasksList = new String[0][3];
+        String[] tokens = new String[3];
+        try {
+            Scanner fileScanner = new Scanner(file);
+            while (fileScanner.hasNextLine()) {
+                line = fileScanner.nextLine();
+                tokens = line.split(", ");
+                tasksList = Arrays.copyOf(tasksList, tasksList.length + 1);
+                tasksList[tasksList.length - 1] = tokens;
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File your're looking for does not exist." +
+                    "\nSee localized message of the exception -> e.getLocalizedMessage() = " + e.getLocalizedMessage());
+        }
+        return tasksList;
     }
 }
