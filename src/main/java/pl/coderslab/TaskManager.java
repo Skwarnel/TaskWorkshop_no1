a9aeb6c57c5abb258c4ba7434bbf95f15d8f7b74
Griegoriens <skwarnel@gmail.com>
2022-08-15 16:45:05 +0200

    Finished: adding task to the array list.

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -8,33 +8,29 @@ import java.util.Scanner;
 public class TaskManager {
     //developed on main branch
     public static void main(String[] args) {
-//        String[][] tasksList = new String[0][0];
-//        tasksList = writeTasksToArray("target/tasks.csv");
-//        printTasks(tasksList);
         mainRun();
-
-
     }
 
     public static void mainRun() {
         String[][] tasksList = new String[0][0];
-        tasksList = writeTasksToArray("target/tasks.csv");
-        printTasks(tasksList);
+        tasksList = writeCsvTasksToArray();
 
         Scanner scan = new Scanner(System.in);
         String line = "";
+
         String[] options = {"add", "remove", "list", "select"};
         printOptions(options);
         while (!(line = scan.nextLine()).equals("quit")) {
             switch (line) {
                 case "add":
-                    System.out.println("invoke addTask();");
+                    tasksList = addTask(tasksList);
+                    printOptions(options);
                     break;
                 case "remove":
                     System.out.println("invoke removeTask();");
                     break;
                 case "list":
-                    System.out.println("invoke listTask();");
+                    printTasks(tasksList);
                     break;
                 case "select":
                     System.out.println("invoke selectTask();");
@@ -54,20 +50,21 @@ public class TaskManager {
     }
 
     public static void useCorrectPrtMsg(String[] arr) {
-        System.out.println(ConsoleColors.PURPLE_BOLD + "Please, use correct command: ");
+        System.out.println(ConsoleColors.PURPLE_BOLD + "Something is not going well. Please, do not isolate yourself in the big depression, just use correct command and enjoy: ");
         for (int i = 0; i < arr.length; i++) {
             System.out.println(ConsoleColors.CYAN_BRIGHT + " -- " + arr[i] + ConsoleColors.RESET);
         }
     }
 
-    public static void printTasks(String [][] tasksList) {
+    public static void printTasks(String[][] tasksList) {
         for (int i = 0; i < tasksList.length; i++) {
-            System.out.println("Arrays.toString(tasksList[i]) = " + Arrays.toString(tasksList[i]));
+            System.out.println("Task no: " + (i + 1) + "   --->   " + Arrays.toString(tasksList[i]));
         }
-        System.out.println("tasksList.length = " + tasksList.length);
+        System.out.println("Tasks quantity = " + tasksList.length);
     }
 
-    public static String[][] writeTasksToArray(String fileName) {
+    public static String[][] writeCsvTasksToArray() {
+        String fileName = "target/tasks.csv";
         File file = new File(fileName);
         String line;
         Scanner scan = new Scanner(System.in);
@@ -83,9 +80,45 @@ public class TaskManager {
             }
 
         } catch (FileNotFoundException e) {
-            System.out.println("File your're looking for does not exist." +
-                    "\nSee localized message of the exception -> e.getLocalizedMessage() = " + e.getLocalizedMessage());
+            System.out.println(ConsoleColors.RED_BOLD + "File with database of your tasks does not exist.\n" +
+                    "Please don't panic and just check the path: " + "'' " + fileName + " ''" +
+                    "\nIf anything to you seems be entirely out of your expectations, just contact with the developer of this software." +
+                    "\nSee localized message of the exception -> e.getLocalizedMessage() = " + e.getLocalizedMessage()
+                    + ConsoleColors.RESET);
         }
         return tasksList;
     }
+
+    public static String[][] addTask(String[][] listTasks) {
+        Scanner scan = new Scanner(System.in);
+        String[] token = new String[3];
+        listTasks = Arrays.copyOf(listTasks, listTasks.length + 1);
+        System.out.println("listTasks.length = " + listTasks.length);
+
+        System.out.println(ConsoleColors.BLUE_BRIGHT + "Please add task description:" + ConsoleColors.RESET);
+        token[0] = scan.nextLine();
+
+        System.out.println(ConsoleColors.GREEN + "Please add task due date:" + ConsoleColors.RESET);
+        token[1] = scan.nextLine();
+
+        System.out.println(ConsoleColors.CYAN + "Is your task urgent? Please select 'true' or 'false':" + ConsoleColors.RESET);
+        token[2] = scan.nextLine();
+
+        listTasks[listTasks.length - 1] = token;
+        System.out.println(ConsoleColors.YELLOW_BOLD + "Task added with success." + ConsoleColors.RESET);
+        return listTasks;
+    }
+
+    public static void removeTask() {
+
+    }
+
+    public static void listTask() {
+
+    }
+
+    public static void selectTask() {
+
+    }
+
 }
